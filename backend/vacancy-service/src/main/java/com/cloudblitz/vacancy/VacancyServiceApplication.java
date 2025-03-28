15b019f05d49f85e00b6aade9d21b73e2cac0ee6
skalsait <skalsait@dyl.com>
2025-03-28 17:49:53 +0530

    update main

--- backend/vacancy-service/src/main/java/com/cloudblitz/vacancy/VacancyServiceApplication.java
+++ backend/vacancy-service/src/main/java/com/cloudblitz/vacancy/VacancyServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CareerSiteApplication {
+public class VacancyServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CareerSiteApplication.class, args);
+        SpringApplication.run(VacancyServiceApplication.class, args);
     }
 }
